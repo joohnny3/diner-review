79cdf97e772fda41bbe29c01195a1fc33015907a
Joohnny <johnny31258@gmail.com>
2023-08-15 15:54:47 +0800

    🗃️ 建立Model關聯(hasMany/belongsTo)

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+
+    public function diner()
+    {
+        return $this->belongsTo(Diner::class);
+    }
 }
