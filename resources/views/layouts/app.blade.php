f6c8e77750ca3217fe5604d0b50f3240e73644b9
Joohnny <johnny31258@gmail.com>
2023-08-16 16:07:33 +0800

    💄 新增網頁標題icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <title>Restaurants</title>
+  <link rel="icon" href="{{ asset('image/cutlery.png') }}" type="image/png">
   <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio"></script>
 
   {{-- blade-formatter-disable --}}
