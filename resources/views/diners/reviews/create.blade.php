1c165d6dcd839833cc0b9ee645920d8dd23400a6
Joohnny <johnny31258@gmail.com>
2023-08-21 10:22:55 +0800

    💄 評分樣式調整及新增取消按鈕

--- resources/views/diners/reviews/create.blade.php
+++ resources/views/diners/reviews/create.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="mb-10 text-2xl">{{ $diner->title }} </h1>
+    <h1 class="mb-3 text-2xl">{{ $diner->title }} </h1>
     <form action="{{ route('diners.reviews.store', $diner) }}" method="POST">
         @csrf
-        <label for="rating"></label>
+        {{-- <label for="rating"></label>
         <select name="rating" id="rating" class="input mb-4" required>
             <option value="">選擇分數</option>
             @for ($i = 1; $i <= 5; $i++)
@@ -12,12 +12,35 @@
                     {{ $i }}
                 </option>
             @endfor
-        </select>
+        </select> --}}
+        <div class="stars mb-8 text-center">
+            @for ($i = 1; $i <= 5; $i++)
+                <span class="star text-3xl text-gray-400 cursor-pointer" style="padding:0 8px"
+                    data-value="{{ $i }}"><i class="fa-solid fa-star"></i></span>
+            @endfor
+            <input type="hidden" name="rating" id="rating" value="">
+        </div>
         <label for="review"></label>
         <textarea name="review" placeholder="詳細說明你在這間餐廳的親身體驗" id="review" required class="input mb-4" style="height:250px"></textarea>
 
         <div class="flex justify-end">
+            <button type="button" class="btn mr-3" onclick="window.history.back();">取消</button>
             <button type="submit" class="btn">張貼</button>
         </div>
     </form>
+    <script>
+        document.querySelectorAll('.star').forEach(star => {
+            star.addEventListener('click', function(e) {
+                let value = this.dataset.value;
+                document.getElementById('rating').value = value;
+
+                document.querySelectorAll('.star').forEach(s => {
+                    s.classList.remove('selected');
+                });
+                for (let i = 1; i <= value; i++) {
+                    document.querySelector(`.star[data-value="${i}"]`).classList.add('selected');
+                }
+            });
+        });
+    </script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,8 +9,8 @@
     <script src="https://kit.fontawesome.com/59074ffc1b.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
 
     {{-- blade-formatter-disable --}}
   <style type="text/tailwindcss">
@@ -64,6 +64,10 @@
 
     .reset-link {
       @apply text-slate-700 no-underline;
+    }
+
+    .star.selected {
+        color:#f6d813;
     }
   </style>
   {{-- blade-formatter-enable --}}
